374d1cf2c10023af08d7f6f243f5a84070343c57
dawid <dawulf97@gmail.com>
2020-03-17 16:58:17 +0100

    Finish up with update validation and delete

--- src/main/java/com/javasolution/app/ppmtool/services/ProjectTaskService.java
+++ src/main/java/com/javasolution/app/ppmtool/services/ProjectTaskService.java
@@ -9,6 +9,7 @@ import com.javasolution.app.ppmtool.repositories.ProjectRepository;
 import com.javasolution.app.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class ProjectTaskService {
@@ -89,8 +90,19 @@ public class ProjectTaskService {
     }
 
     public ProjectTask updateByProjectSequence(ProjectTask updatedTask,String backlog_id, String pt_id){
-        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id,pt_id);
         projectTask=updatedTask;
         return projectTaskRepository.save(projectTask);
+    }
+
+    public void deletePTByProjectSequence(String backlog_id, String pt_id){
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id,pt_id);
+
+        Backlog backlog = projectTask.getBacklog();
+        List<ProjectTask> pts = backlog.getProjectTasks();
+        pts.remove(projectTask);
+        backlogRepository.save(backlog);
+
+        projectTaskRepository.delete(projectTask);
     }
 }
