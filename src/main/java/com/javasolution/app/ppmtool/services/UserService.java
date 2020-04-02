52096d4fccbbdb8cacf8c927206152cb128575f5
dawid <dawulf97@gmail.com>
2020-04-02 22:13:21 +0200

    User registration2

--- src/main/java/com/javasolution/app/ppmtool/services/UserService.java
+++ src/main/java/com/javasolution/app/ppmtool/services/UserService.java
@@ -1,6 +1,7 @@
 package com.javasolution.app.ppmtool.services;
 
 import com.javasolution.app.ppmtool.domain.User;
+import com.javasolution.app.ppmtool.exceptions.UsernameAlreadyExistsException;
 import com.javasolution.app.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,11 +17,17 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User saveUser(User newUser){
-        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
-        //username has to be unique(exception)
-        //make sure that password and confirmPassword match
-        //we dont persist or show the confirmPassword
-        return userRepository.save(newUser);
+        try{
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+            //username has to be unique(exception)
+            //make sure that password and confirmPassword match
+            //we dont persist or show the confirmPassword
+            return userRepository.save(newUser);
+        }catch(Exception e){
+            throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
+        }
+
     }
 }
