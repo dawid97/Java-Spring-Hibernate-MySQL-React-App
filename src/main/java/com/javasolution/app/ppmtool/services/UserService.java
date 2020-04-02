7f6f864a4ca0e9a3be2e93659540bfb0008068b4
dawid <dawulf97@gmail.com>
2020-04-02 17:54:17 +0200

    User registration1

--- src/main/java/com/javasolution/app/ppmtool/services/UserService.java
+++ src/main/java/com/javasolution/app/ppmtool/services/UserService.java
@@ -1,7 +1,9 @@
 package com.javasolution.app.ppmtool.services;
 
+import com.javasolution.app.ppmtool.domain.User;
 import com.javasolution.app.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +11,16 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User newUser){
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+        //username has to be unique(exception)
+        //make sure that password and confirmPassword match
+        //we dont persist or show the confirmPassword
+        return userRepository.save(newUser);
+    }
 }
